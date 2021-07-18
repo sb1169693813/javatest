3d23796a825cbb7c1d97e811d7d4e21bd01e8c37
sb1169693813 <446352377@qq.com>
2021-07-18 14:01:13 +0800

    孙斌的spring boot学习

---
+++ src/main/java/com/sunspringboot/test/SunspringboottestApplication.java
@@ -0,0 +1,13 @@
+package com.sunspringboot.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunspringboottestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunspringboottestApplication.class, args);
+	}
+
+}
